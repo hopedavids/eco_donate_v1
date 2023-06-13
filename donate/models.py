698bc232b00ecde3ff13846cea0f77cff79ebbba
hopedavids <hledavids@gmail.com>
2023-06-13 16:02:47 +0000

    defined the database schemas

--- donate/models.py
+++ donate/models.py
@@ -6,4 +6,14 @@ from .donate_app import db
 """
 
 
-class 
+class User(db.Model):
+    """In this class, the data model for the User
+        will be defined.
+    """
+
+    __tablename__ = 'donate_users'
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False, unique=True)
