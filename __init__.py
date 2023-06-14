bab11c01ba5d758473f53e808ed97270c32b06bc
emmanueldavids <emanueldavids417@gmai.com>
2023-06-14 14:41:04 +0000

    added url_for

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -45,3 +45,6 @@ def single_portfolio():
 @app.route('/single-post')
 def single_post():
     return render_template('single-post.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
