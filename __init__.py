8fe56a32cc5d2cd236176311231487d09190967f
emmanueldavids <emanueldavids417@gmai.com>
2023-06-16 22:04:19 +0000

    removed some functions from the __init__.py

--- __init__.py
+++ __init__.py
@@ -10,18 +10,6 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
-
-@app.route('/cart')
-def cart():
-    return render_template('cart.html')
-
-@app.route('/checkout')
-def checkout():
-    return render_template('checkout.html')
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
@@ -29,14 +17,6 @@ def contact():
 @app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html')
-
-@app.route('/shop_details')
-def shop_details():
-    return render_template('shop-details.html')
-
-@app.route('/shop')
-def shop():
-    return render_template('shop.html')
 
 @app.route('/single-portfolio')
 def single_portfolio():
