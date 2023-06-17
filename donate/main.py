868e1cd0547cf8e6273b2f6008ab0772d1a8e467
hopedavids <hledavids@gmail.com>
2023-06-17 10:42:14 +0000

    added the login_required method

--- donate/main.py
+++ donate/main.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__)
 
 @main.route('/landing_page')
+@login_required
 def index():
     return render_template('backend/index.html')
