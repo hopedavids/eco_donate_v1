4f2667a6a81ef9376469a8c0d8a57be486e08665
hopedavids <hledavids@gmail.com>
2023-07-04 16:11:05 +0000

    defined the user creation api model

--- donate/api_models.py
+++ donate/api_models.py
@@ -22,6 +22,16 @@ user_input_model = api.model(
     }
 )
 
+
+user_creation_model = api.model(
+    "User", {
+        "username": fields.String(required=True),
+        "email": fields.String(required=True),
+        "password": fields.String(required=True),
+    }
+)
+
+
 api_auth =  {
         "username": fields.String,
         "email": fields.String,
@@ -41,11 +51,38 @@ wallet_model = api.model(
     }
 )
 
+
+contact_model = api.model(
+    "Contact", {
+        "contact_id": fields.Integer,
+        "user": fields.Nested(user_model),
+        "full_name": fields.String,
+        "address": fields.String,
+        "country": fields.String,
+        "about_me": fields.String
+    }
+)
+
+
+donation_model = api.model(
+    "Donation", {
+        "donation_id": fields.Integer,
+        "user": fields.Nested(user_model),
+        "amount": fields.Float,
+        "tree_spieces": fields.String,
+        "number_of_trees": fields.String,
+        "region_to_plant": fields.String,
+        "description": fields.String,
+        "get_certified": fields.Boolean,
+        "timestamp": fields.DateTime(dt_format='iso8601')
+    }
+)
+
 payment_model = api.model(
     "Payment", {
         "payment_id": fields.Integer,
         "wallet_id": fields.String,
-        "donation_id": fields.Integer,
+        "donation": fields.Nested(donation_model),
         "amount": fields.Float,
         "timestamp": fields.DateTime(dt_format='iso8601')
     }
