be9957849e550f0ec0b2143dff68091ee595e850
hopedavids <hledavids@gmail.com>
2023-07-05 19:46:16 +0000

    implementing the api resources

--- donate/resources.py
+++ donate/resources.py
@@ -53,15 +53,14 @@ class Authentication(Resource):
         pass
 
 
-@user_ns.route('/get_user')
+@user_ns.route('')
 class Users(Resource):
-    """This class object defines the routes and views for
-       User Authentication.
+    """This defines the routes and views for
+       POST and GET requests for the User Object.
     """
 
     # @user_ns.doc(security="jsonWebToken")
     @user_ns.marshal_list_with(user_model)
-    # @jwt_required()
     def get(self):
         """ This method handles the GET HTTP method and returns
             response in a serialized way.
@@ -77,83 +76,188 @@ class Users(Resource):
         """This method handles the POST and creates new uses based
             models defined.
         """
-        # Access the request payload data using `request.get_json()`
-        payload = request.get_json()
-        
-        # Validate the payload data against the user_creation_model
-        if not api.payload:
-            return {'message': 'Invalid payload'}, 400
-        
-        # Extract the data from the payload using marshal
-        username = api.payload.get('username')
-        email = api.payload.get('email')
-        password = api.payload.get('password')
+        try:
+            # Access the request payload data using `request.get_json()`
+            payload = request.get_json()
+            
+            # Validate the payload data against the user_creation_model
+            if not api.payload:
+                return {'message': 'Invalid payload'}, 400
+            
+            
+            # Extract the data from the payload using marshal
+            username = api.payload['username']
+            email = api.payload['email']
+            password = api.payload['password']
 
-        # hashed the plain text password with the generate_password_hash method
-        hashed_password = generate_password_hash(password, method='sha256')
+            # hashed the plain text password with the generate_password_hash method
+            hashed_password = generate_password_hash(password, method='sha256')
 
-        # inject the data into the User object
-        user = User(username=username, email=email, password=hashed_password)
-        
-        # save the new instance into the database
-        db.session.add(user)
-        db.session.commit()
+            user = User.query.filter_by(username=username).first()
 
-        # return successful message once passed
-        return {'message': 'User created successfully'}, 201
+            if not username or  not email or not password:
+                return {'message': 'Invalid fields'}, 400
+
+            if '@' not in email or '@' in username:
+                return {'message': 'Invalid Email or Username'}, 400
+            
+            if len(password) < 12:
+                return {'message': 'password length is too short and is vunerable to bruteforce attacks'}, 400
+            
+            if not any(char.isalpha() for char in password) or not any(char.isdigit() for char in password):
+                return {'message', 'sorry, your password isn\'t  strong enough'}, 400
+            
+
+            # inject the data into the User object
+            user = User(username=username, email=email, password=hashed_password)
+            
+            # save the new instance into the database
+            db.session.add(user)
+            db.session.commit()
+
+            # The wallets is created automatically
+            wallet = Wallet(user_id=user.id)
+            db.session.add(wallet)
+            db.session.commit()
+
+            # return successful message once passed
+            return {'message': 'User created successfully'}, 201
+
+        except:
+            return {'message': 'User creation failed! Try again'}, 400
+    
 
 
-    def put(self):
-        """This method updates a user details and roles"""
-
-        pass
 
 
 
-@user_ns.route('/<int:id>')
+@user_ns.route('/<int:userid>')
 class Users(Resource):
     """This class object defines the routes and views for
        User Authentication.
     """
 
     @user_ns.marshal_list_with(user_model)
-    def get(self, id):
-        """ This method handles the GET HTTP method and returns
-            response in a serialized way.
+    def get(self, userid):
+        """ This method handles the GET HTTP method 
+            and returns response in a serialized format.
         """
         #query the user object
-        user = User.query.filter_by(id=id).first()
-
+        user = User.query.filter_by(id=userid).first()
+        # return the user object with 200 StatusCode
         return user, 200
     
-    
+    @user_ns.expect(user_creation_model)
+    def put(self, userid):
+        """ This method handles the PUT HTTP method and returns 
+            the updated instance of the object 
+        """
+        try:
+            payload = request.get_json()
 
-@wallet_ns.route('/all-wallets')
+            # query the User object with a given user id
+            user = User.query.filter_by(id=userid).first()
+
+            if not user or ('username' not in payload and 'email' not in payload):
+                return {
+                    'data': 'null',
+                    'message': 'User not found or missing username/email',
+                    'status': 'api-error'
+                }, 400
+
+            # update username if exists in payload
+            if 'username' in payload:
+                user.username = payload['username']
+            
+            # update email if exists in payload
+            if 'email' in payload:
+                user.email = payload['email']
+            
+            if 'email' in payload and '@' not in payload['email']:
+                return {
+                    'message': 'Email format is not valid',
+                    'status': 'api-error'
+                }, 400
+            
+            # add and save the User instance to the database
+            db.session.commit()
+
+            return ({
+                'message': 'user details has been updated successfully',
+                'status': 'success'
+            }), 201
+
+
+        except Exception as e:
+            return {
+                'data': 'Null',
+                'message': 'Error: {}'.format(str(e)),
+                'status': 'api-error'
+            }, 400
+
+
+    def delete(self, userid):
+        """ This method handles the DELETE HTTP method 
+            and returns.
+        """
+        try:
+            user = User.query.filter_by(id=userid).first()
+
+            if not user:
+                return ({
+                        'message': 'User Id entered is not valid',
+                        'status': 'api-error'
+                    }), 400
+            
+            
+            db.session.delete(user)
+            db.session.commit()
+
+            return ({
+                    'message': 'user has been deleted successfully',
+                    'status': 'success'
+                }), 201
+
+        except Exception as e:
+            return {
+                'data': 'Null',
+                'message': 'Error: {}'.format(str(e)),
+                'status': 'api-error'
+            }, 400
+
+
+
+@wallet_ns.route('')
 class Wallet_Details(Resource):
-    """This object defines the routes and views for Wallet and
-        handles all wallets resources.
+    """This object defines the routes and views for GET requests
+        to this endpoint.
     """
 
     @wallet_ns.marshal_list_with(wallet_model)
     def get(self):
-        """The get method handles the HTTP GET requests and returns
+        """The get method handles generic HTTP GET requests and returns
             response in a serialized way.
         """
 
         wallet = Wallet.query.all()
         return wallet
 
-    def post(self):
-        """This method provides the means to create new wallets."""
 
-        pass
-
-    def put(self):
-        """This method provides the flexibility to update user's
-            wallet details.
+@wallet_ns.route('/<int:userid>')
+class Wallet_Details(Resource):
+    """ This object defines the routes and views for specific GET
+        and PUT requests for this endpoint.
+    """
+    
+    @wallet_ns.marshal_list_with(wallet_model)
+    def get(self, userid):
+        """The get method filters for specific userid using HTTP GET
+            requests and returns a serialized results.
         """
 
-        pass
+        wallet = Wallet.query.filter_by(user_id=userid).first()
+
+        return wallet, 200
 
 
 @pay_ns.route('/all-payments')
