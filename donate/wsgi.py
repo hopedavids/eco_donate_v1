ddeb679f784ec6d2bffdbf08ef207bb2172f6e79
hopedavids <hledavids@gmail.com>
2023-07-11 21:50:25 +0000

    fixed the bug in wsgi

--- donate/wsgi.py
+++ donate/wsgi.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add the root directory of your project to the Python path
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(ROOT_DIR)
+
 from donate_app import create_app
 
 
